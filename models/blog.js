2e3141c9b696e888cc5c741a905f216a08b0f58f
star407 <starmatka786@gmail.com>
2023-09-25 11:17:51 +0530

    twelve

--- models/blog.js
+++ models/blog.js
@@ -1,21 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const dataSchema = new mongoose.Schema({
-    entrydate: {
-      type: String,
-      unique: true,
-      required:true
-    },
-    title: {
-      type: String,
-      required: true
-    },
-    patti1: {
-      type: [Number]
-    },
-    patti2: {
-      type: [Number]
-    }
-  });
+  entrydate: {type: String, unique: true, required: true},
+  title: {type: String, required: true},
+  patti1: {type: [Number]},
+  patti2: {type: [Number]}
+});
 const data = mongoose.model('patti', dataSchema);
 
 module.exports = data
