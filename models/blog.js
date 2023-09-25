c30298113d6b4e9c33785b6d00ccf3b6c8fe48ee
star407 <starmatka786@gmail.com>
2023-09-25 10:01:08 +0530

    eleven

--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose")
 const dataSchema = new mongoose.Schema({
-    email: {
+    entrydate: {
       type: String,
-      unique: true // `email` must be unique
+      unique: true,
+      required:true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    patti1: {
+      type: [Number]
+    },
+    patti2: {
+      type: [Number]
     }
   });
-const data = mongoose.model('db', dataSchema);
+const data = mongoose.model('patti', dataSchema);
 
 module.exports = data
