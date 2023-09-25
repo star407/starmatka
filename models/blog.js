edba2afa0ec261254a2a7fc7727c4ac8eb3229d1
star407 <starmatka786@gmail.com>
2023-09-25 12:12:08 +0530

    twelve

--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const dataSchema = new mongoose.Schema({
-  entrydate: {type: String, unique: true, required: true},
+  entrydate: {type: String, required: true},
   title: {type: String, required: true},
   patti1: {type: [Number]},
   patti2: {type: [Number]}
-});
+},{ "strict": false })
+dataSchema.index({ "entrydate": 1, "title": 1}, { "unique": true });
 const data = mongoose.model('patti', dataSchema);
 
 module.exports = data
